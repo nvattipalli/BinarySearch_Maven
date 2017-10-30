11c01b3f18343a6cba73ae8a30600c999ec656ae
nvattipalli <31747094+nvattipalli@users.noreply.github.com>
2017-10-30 15:28:45 -0500

    Update binarySearch.java

--- src/main/java/hw3/binarysearch/binarySearch.java
+++ src/main/java/hw3/binarysearch/binarySearch.java
@@ -9,92 +9,89 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-public class binarySearch {
-	 public static final int NOT_FOUND = -1;
-	public static int binarySearch( Comparable [ ] a, Comparable x )
-    {
-        int low = 0;
-        int high = a.length - 1;
-        int mid;
-
-        while( low <= high )
-        {
-            mid = ( low + high ) / 2;
-
-            if( a[ mid ].compareTo( x ) < 0 )
-                low = mid + 1;
-            else if( a[ mid ].compareTo( x ) > 0 )
-                high = mid - 1;
-            else
-                return mid;
-        }
-
-        return NOT_FOUND;
-    }
-
-
- public static void main(String[] args) throws Exception {
-	   
-    	
-     
-  Options options = new Options();
-
-  Option input = new Option("type", "input", true, "input file to read data from");
-  input.setRequired(true);
-  input.setArgName("FILE PATH");
-  options.addOption(input);
-
-  Option output = new Option("key", "output", true, "output file to write the final result");
-  output.setRequired(true);
-  output.setArgName("FILE PATH");
-  options.addOption(output);
-  
- Option output2 = new Option("list", "output2", true, "output file to write the final result");
-  output2.setRequired(true);
-  output2.setArgName("FILE PATH");
-  options.addOption(output2);
-
-  CommandLineParser commandLineParser = new DefaultParser();
-
-  CommandLine commandLine;
-try{
-  commandLine = commandLineParser.parse(options, args);
-
-  String inputFilePath = commandLine.getOptionValue("input");
-  String outputFilePath = commandLine.getOptionValue("output");
-  String firstelement = commandLine.getOptionValue("output2");
-  List<String> list = commandLine.getArgList();
-  
-  
-  String [ ] a1 = new String [list.size()+1];
-  a1[0]=firstelement;
-  for (int i=1;i<list.size()+1;i++)
-	  a1[i]=list.get(i-1);
- 
-  if(inputFilePath.compareTo("s")==0)
-  {
-	  int f = binarySearch(a1,outputFilePath);
-	  if(f == -1)
-		  System.out.println("0");
-	  else
-		  System.out.println("1");
-  }
-  else if(inputFilePath.compareTo("i")==0)
-  {
-	  int f = binarySearch(a1,outputFilePath);
-  if(f == -1)
-	  System.out.println("0");
-  else
-	  System.out.println("1");
-  }
-  else
-	  System.out.println("Invalid Type.Please enter i for integers and s for strings");
+public class binarySearch  {
+	
+	public static void main(String[] args) {
+		int returnValue = -1;
+		
+		Options options = new Options();
+		Option typeOption = Option.builder("t").required().longOpt("type").hasArg().argName("TYPE").build();
+		options.addOption( typeOption );
+		
+		Option keyOption = Option.builder("k").required().longOpt("key").hasArg().argName("KEY").build();
+		options.addOption( keyOption );	
+				
+		Option listOption = Option.builder("l").required().longOpt("list").hasArgs().argName("LIST").build();
+		options.addOption( listOption );
+		
+		
+		CommandLineParser parser = new DefaultParser();	
+		try {	
+		    
+		    CommandLine line = parser.parse( options, args );
+		    String inputType = line.getOptionValue("type");
+		    String inputKey = line.getOptionValue("key");
+		    String[] inputList = line.getOptionValues("list");
+		    if (inputType.equalsIgnoreCase("i")) {
+		    	
+		    	try {											
+		    		Integer keyValue=Integer.parseInt(inputKey);
+			    	int length= inputList.length;
+			    	Integer[] listCopy= new Integer[length];
+		    		for (int i = 0; i < length; i++) {
+		    			listCopy[i]=Integer.parseInt(inputList[i]); 
+			    	}
+		    		
+			    	if (binSearch(listCopy,keyValue)){
+			    		returnValue = 1;
+			    	}
+			    	else {
+			    		returnValue = 0;
+			    	}
+		    	} 
+		    	catch (NumberFormatException nume) {
+		    		    System.out.println("Incorrect Input! Exception: " + nume);
+		    		    System.exit(1);
+		    	}
+		    }
+		    else if (inputType.equalsIgnoreCase("s")) {
+		    	String keyValue=inputKey;
+		    	String[] listCopy=inputList;
+		    	if (binSearch(listCopy,keyValue)){
+		    		returnValue = 1;
+		    	}
+		    	else {
+		    		returnValue = 0;
+		    	}
+		    }
+		    System.out.println(returnValue);				
+		}
+		catch( ParseException e ) {
+		    System.out.println("Unexpected exception:" + e.getMessage());
+		}
+		
 	}
-
-  catch( ParseException exp ) {
-	    System.out.println( "Unexpected exception:" + exp.getMessage() );
+		
+	private static <T extends Comparable<T>> boolean binSearch(T[] aList, T key) {
+		int first = 0;						
+		int last = aList.length - 1;		
+		int mid = (first + last)/2;
+		T middleValue=aList[mid];
+		while (first <= last) {			
+			if(key.compareTo(middleValue)==0){
+				return true;				
+			}
+			else if (key.compareTo(middleValue)<0){
+				
+				last = mid-1	;		
+			}
+			else if (key.compareTo(middleValue)>0){
+				first = mid+1;		
+				
+			}
+			mid=(first+last)/2;
+			middleValue=aList[mid];
+		}
+		return false;
 	}
- 
- }
- 
- }
+}
